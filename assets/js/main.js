da0cac61c9d0114da782c2a216ebb9f763f2d6c6
Aref <arefmaleky@gmail.com>
2021-12-22 17:03:03 +0330

    add_png-files_add-some-sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,12 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+        // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
